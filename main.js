7a90cce1cace20afd9092692526c01cdf4d60c12
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-08-15 23:41:57 +0100

    theming

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, Menu, BrowserWindow} = require('electron')
 const path = require('path')
+
 
 function createWindow () {
   // Create the browser window.
