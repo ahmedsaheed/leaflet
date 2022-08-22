ad8db5ce431d5086014892c98d4c8352c5a02930
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-08-22 13:41:38 +0100

    add code block highlighting

--- renderer/pages/next.jsx
+++ renderer/pages/next.jsx
@@ -1,102 +1,70 @@
 import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
 import { Remarkable } from 'remarkable';
 import { markdownToHtml } from '../lib/mdToHtml.ts';
 import {useState} from 'react';
+import hljs from 'highlight.js'
 
-// convert the below class into a functional component
-const Next =  () => {
-  const [value, setValue] = useState("Hello **World!**");
-  const [html, setHtml] = useState("");
 
-  const handleChange = (e) =>  {
-    setValue(e.target.value);
-     setHtml(markdownToHtml(e.target.value));
-    console.log(html);
+class Next extends React.Component {
+  constructor(props) {
+    super(props);
+     
+ this.md = new Remarkable({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (err) {}
+    }
+
+    try {
+      return hljs.highlightAuto(str).value;
+    } catch (err) {}
+
+    return ''; // use external default escaping
+  }
+});
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { value: 'Hello, **world**!' };
   }
 
-  return (
-    <div className="MarkdownEditor">
-      <div style = {{float: "left", maxWidth: "50vw", padding: "2em"}}>
-      <h3>Input</h3>
-      <label htmlFor="markdown-content">
-        Enter some markdown
-      </label>
-      <textarea
-        id="markdown-content"
-        onChange={handleChange}
-        defaultValue={value}
-        className=" h-full w-full"
-        style={{ marginTop: "2em", minHeight: "60vh", backgroundColor: 'transparent', border: '1px solid #ccc', borderRadius: '4px'}} 
-      />
-      </div>
-      <div style={{float: "right", minWidth: "50vw", minHeight: "100vh", border: '1px solid #ccc', padding: "2em"}}>
-      <h3>Output</h3>
-      <div
-          style={{marginTop: "2em"}}
-        className="content list-decimal"
-        dangerouslySetInnerHTML={ {__html: html} }
-      />
-      </div>
-    </div>
-  );
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  getRawMarkup() {
+    return { __html: this.md.render(this.state.value) };
+  }
 
 
+  render() {
+    return (
+      <div className="MarkdownEditor">
+        <div style = {{float: "left", maxWidth: "50vw", padding: "2em"}}>
+        <h3>Input</h3>
+        <label htmlFor="markdown-content">
+          Enter some markdown
+        </label>
+        <textarea
+          id="markdown-content"
+          onChange={this.handleChange}
+          defaultValue={this.state.value}
+          className=" h-full w-full"
+          style={{ marginTop: "2em", minHeight: "60vh", backgroundColor: 'transparent', border: '1px solid #ccc', borderRadius: '4px'}} 
+        />
+        </div>
+        <div style={{float: "right", minWidth: "50vw", minHeight: "100vh", border: '1px solid #ccc', padding: "2em"}}>
+        <h3>Output</h3>
+        <div
+            style={{marginTop: "2em"}}
+          className="content list-decimal"
+          dangerouslySetInnerHTML={ this.getRawMarkup()}
+        />
+        </div>
+      </div>
+    );
+  }
 }
-
-// class Next extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.md = new Remarkable();
-//     this.handleChange = this.handleChange.bind(this);
-//     this.state = { value: 'Hello, **world**!' };
-//   }
-
-//   handleChange(e) {
-//     this.setState({ value: e.target.value });
-//   }
-
-
-//   async getContent() {
-//     const content = await markdownToHtml(this.state.value || "")
-//     console.log(content);
-
-//     return content;
-//   }
-
-//   //   async getRawMarkup() {
-//   //    return { __html:  await this.getContent() };
-//   // }
-
-//   render() {
-//     return (
-//       <div className="MarkdownEditor">
-//         <div style = {{float: "left", maxWidth: "50vw", padding: "2em"}}>
-//         <h3>Input</h3>
-//         <label htmlFor="markdown-content">
-//           Enter some markdown
-//         </label>
-//         <textarea
-//           id="markdown-content"
-//           onChange={this.handleChange}
-//           defaultValue={this.state.value}
-//           className=" h-full w-full"
-//           style={{ marginTop: "2em", minHeight: "60vh", backgroundColor: 'transparent', border: '1px solid #ccc', borderRadius: '4px'}} 
-//         />
-//         </div>
-//         <div style={{float: "right", minWidth: "50vw", minHeight: "100vh", border: '1px solid #ccc', padding: "2em"}}>
-//         <h3>Output</h3>
-//         <div
-//             style={{marginTop: "2em"}}
-//           className="content list-decimal"
-//           dangerouslySetInnerHTML={ {__html: this.getContent()}}
-//         />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 
 
