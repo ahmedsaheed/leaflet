d96bec19bc5483b0804be16d6cb667b8054b6f85
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-08-23 02:29:21 +0100

    more update

--- renderer/components/buttomBar.jsx
+++ renderer/components/buttomBar.jsx
@@ -5,12 +5,12 @@ export default function ButtomBar({word}) {
 const date = new Date();
     return(
         <div className="fixed inset-x-0 bottom-0 ButtomBar" style={{marginLeft: "30%", maxHeight: "10vh"}}>
-        <container className="Left" style={{float: "left"}}>
+        <container className="Left" style={{float: "left", paddingLeft: "40px"}}>
             <span>1:1</span>
             <div style={{display: "inline", marginRight:"40px"}}></div>
             <span>{Countable(word)}</span>
         </container>
-        <container className="Right" style={{float: "right"}}>
+        <container className="Right" style={{float: "right", paddingRight: "40px"}}>
          <span style={{float: "left"}}>note</span>
          <div style={{display: "inline", marginLeft: "40px"}}></div>
         <span style={{float: "right"}}>{date.getHours()}:{date.getMinutes()}</span>
