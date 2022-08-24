7d677e5ccb8fae97fa6162053d337db797a1e093
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-08-24 21:48:19 +0100

    render files and its content correctky

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
 import { ipcRenderer } from "electron";
 import {progress} from "../components/progress.ts";
-import {getMarkdown} from "../lib/mdParser";
+// import {getMarkdown} from "../lib/mdParser";
 import ButtomBar from "../components/buttomBar";
 import Fs from "../components/fs";
 import fs from "fs";
+import { Remarkable } from "remarkable";
+import hljs from "highlight.js";
+import katex from "remarkable-katex";
 
 
 export default function Next() {
@@ -12,7 +15,7 @@ export default function Next() {
   const [isVisble, setIsVisble] = React.useState(false);
   const [scroll, setScroll] = React.useState(0);
   const [files, setFiles] = React.useState([]);
-  const [save, setSave] = React.useState(false);
+  const [name, setName] = React.useState("");
 
   //TODO: IMPLEMENT SAVE FUNCTION
   // useEffect(() => {
@@ -37,10 +40,32 @@ export default function Next() {
   //     );
   // };
 
+  const md = new Remarkable("full", {
+    html: true,
+    typographer: true,
+    highlight: function (str, lang) {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return hljs.highlight(lang, str).value;
+        } catch (err) {}
+      }
+
+      try {
+        return hljs.highlightAuto(str).value;
+      } catch (err) {}
+    },
+  });
+  md.use(katex);
+
+  const getMarkdown =() => {
+    console.log(value.toString());
+    return { __html: md.render(value) };
+  }
   useEffect(() => {
     ipcRenderer.invoke("getTheFile").then((files = []) => {
       setFiles(files);
       setValue(files[0] ? `${files[0].body}` : "");
+      setName(files[0] ? `${files[0].name}` : "");
     });
   }, []);
 
@@ -59,13 +84,6 @@ export default function Next() {
   useEffect(() => {
     document.addEventListener("keydown", detectKeydown, true);
   }, []);
-  //get the index and set the value
-
-  const onFileClick = (index) => {
-    //TODO remember that index starts from one, so when called always -1
-    console.log(files)
-    setValue(files[2].body);
-  }
 
   const detectKeydown = (e) => {
     if (e.key === "k" && (e.ctrlKey || e.metaKey)) {
@@ -100,9 +118,7 @@ export default function Next() {
         <div style={{ marginTop: "2vh", marginBottom: "2vh" }}>
           {files.map((file, index) => (
             <>
-            <button onClick={() => onFileClick(file.index)}><ol className="files">{`${file.name.toString().toUpperCase()}`}</ol></button>
-             
-              <ol className="files">{`${file.index}`}</ol>
+            <ol className="files"><button className={name === file.name ? "selected" : "" } onClick={() => {setValue(file.body); setName(file.name)}}>{`${file.name.toString()}`}</button></ol>
             </>
           ))}
         </div>
@@ -124,7 +140,7 @@ export default function Next() {
               <div
                 style={{ marginTop: "2em", marginBottom: "5em" }}
                 className="third"
-                dangerouslySetInnerHTML={getMarkdown(value)}
+                dangerouslySetInnerHTML={getMarkdown()}
               />
             </>
           ) : (
