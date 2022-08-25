fbe791f59d2264cf868ee7ce08c8827e0db442d8
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-08-25 23:16:54 +0100

    stying to add renderer for chemistry equation

--- renderer/lib/mdParser.ts
+++ renderer/lib/mdParser.ts
@@ -1,5 +1,3 @@
-import { Remarkable } from "remarkable";
-// import {MarkdownIt} from "markdown-it";
 import hljs from "highlight.js";
 
 export const getMarkdown = (value: string) => {  
@@ -32,34 +30,3 @@ export const getMarkdown = (value: string) => {
      
 }
 
-
-
-// import { unified } from "unified"
-// import parse from "remark-parse"
-// import math from "remark-math"
-// import remark2rehype from "remark-rehype"
-// import katex from "rehype-katex"
-// import stringify from "rehype-stringify"
-// import highlight from "rehype-highlight"
-// // import slug from "rehype-slug"
-// // import link from "rehype-autolink-headings"
-// import rehypeRaw from "rehype-raw"
-// import remarkGfm from "remark-gfm"
-
-// export async  function getMarkdown(markdown) {
-//     const result = await  unified()
-//         .use(parse)
-//         .use(math)
-//         .use(remark2rehype, { allowDangerousHtml: true })
-//         .use(remarkGfm)
-//         .use(rehypeRaw)
-//         .use(katex)
-//         .use(stringify)
-//         .use(highlight)
-//         // .use(slug)
-//         .process(markdown.toString());
-
-//         return { __html: result.toString() };
-
-//     // return result.toString()
-// }
