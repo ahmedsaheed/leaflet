d4814f9a4d4db7caf19621be32401e01bc964c7f
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-09-29 00:04:35 +0100

    cleanup finder

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -901,47 +901,8 @@ export default function Next() {
                         />
                       </form>
                   </span>
-                  {/* <div style={{ display: "inline", marginRight: "30px" }}></div>
-                  <span>{`${value.toString().split(" ").length}W ${
-                    value.toString().length
-                  }C `}</span> */}
-                  {/* <div style={{ display: "inline", marginRight: "30px" }}></div> */}
-                  {/* <div
-                    style={{
-                      display: "inline",
-                      color: "grey",
-                      overflow: "hidden",
-                    }}
-                    dangerouslySetInnerHTML={{
-                      __html: insert ? cursor : progress(scroll),
-                    }}
-                  /> */}
                 </container>
-                {/* <container
-                  className="Right"
-                  style={{
-                    float: "right",
-                    paddingRight: "40px",
-                    paddingTop: "5px",
-                    paddingBottom: "5px",
-                  }}
-                >
-                  <span style={{ float: "left" }}>
-                    <svg
-                      style={{ display: "inline" }}
-                      width="32"
-                      height="22"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        fill="#888888"
-                        d="M20.56 18H3.44C2.65 18 2 17.37 2 16.59V7.41C2 6.63 2.65 6 3.44 6h17.12c.79 0 1.44.63 1.44 1.41v9.18c0 .78-.65 1.41-1.44 1.41M6.81 15.19v-3.66l1.92 2.35l1.92-2.35v3.66h1.93V8.81h-1.93l-1.92 2.35l-1.92-2.35H4.89v6.38h1.92M19.69 12h-1.92V8.81h-1.92V12h-1.93l2.89 3.28L19.69 12Z"
-                      />
-                    </svg>
-                  </span>
-                  <div style={{ display: "inline", marginLeft: "20px" }}></div>
-                  {clockState}
-                </container> */}
+            
               </>
 
               ) : (
