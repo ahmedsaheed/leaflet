8ecc4214c982661b75521703fff6688958db0736
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-11-09 18:56:53 +0000

    move fileree to self components

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -5,6 +5,7 @@ import "react-cmdk/dist/cmdk.css";
 import { GETDATE, LINK, BOLD, QUICKINSERT, ADDYAML } from "../lib/util";
 
 import Todo from "../components/todo";
+import  { FileTree } from "../components/filetree";
 
 import {
   COLLAPSEIcon,
@@ -733,6 +734,21 @@ export default function Next() {
     };
   });
 
+
+  const creatingFileOrFolder = () => {
+    if (fileName.length < 1) {
+      setFileNameBox(false);
+      return;
+    }
+    isCreatingFolder
+      ? createNewDir(fileName)
+      : createNewFile();
+    setFileNameBox(false);
+    setTimeout(() => {
+      setFileName("");
+    }, 100);
+  }
+
   const openWindow = () => {
     ipcRenderer.invoke("app:on-fs-dialog-open").then(() => {
       ipcRenderer.invoke("getTheFile").then((files = []) => {
@@ -887,230 +903,24 @@ export default function Next() {
                     textOverflow: "ellipsis",
                   }}
                 >
-                  {struct
-                    .map((file, index) =>
-                      file.children ? (
-                        !fs.existsSync(file.path) ? null : !fs.readdirSync(
-                            file.path
-                          ).length ? null : (
-                          <details key={index} tabIndex={-1}>
-                            <summary
-                              className="files greys"
-                              style={{
-                                outline: "none",
-                                cursor: "pointer",
-                                fontSize: "12px",
-                                fontWeight: "bold",
-                                marginLeft: "1em",
-                              }}
-                              onClick={() => {
-                                setParentDir(file.path);
-                              }}
-                            >
-                              {" "}
-                              {file.name.charAt(0).toUpperCase() +
-                                file.name.slice(1)}
-                            </summary>
-                            {file.children.map((child, index) =>
-                              !fs.existsSync(child.path) ? null : fs
-                                  .statSync(child.path)
-                                  .isDirectory() ? (
-                                !fs.readdirSync(child.path).length ? null : (
-                                  <div
-                                    style={{
-                                      marginLeft: "1.8em",
-                                    }}
-                                  >
-                                    <details key={index} tabIndex={-1}>
-                                      <summary
-                                        className="files greys"
-                                        style={{
-                                          cursor: "pointer",
-                                          whiteSpace: "nowrap",
-                                          overflow: "hidden",
-                                          maxWidth: "100%",
-                                          outline: "none",
-                                          textOverflow: "ellipsis",
-                                        }}
-                                        onClick={() => {
-                                          setParentDir(file.path);
-                                        }}
-                                      >
-                                        {" "}
-                                        {child.name.charAt(0).toUpperCase() +
-                                          child.name.slice(1)}
-                                      </summary>
-                                      {child.children
-                                        .map((child, index) => (
-                                          <ol
-                                            className={
-                                              path === child.path &&
-                                              !fileNameBox
-                                                ? "selected files"
-                                                : "greys files"
-                                            }
-                                            onClick={(e) => {
-                                              onFileTreeClick(
-                                                child.path,
-                                                child.name
-                                              );
-                                            }}
-                                            style={{
-                                              cursor: "pointer",
-                                            }}
-                                          >
-                                            <button
-                                              style={{
-                                                marginLeft: "0.2em",
-                                                whiteSpace: "nowrap",
-                                                overflow: "hidden",
-                                                maxWidth: "100%",
-                                                outline: "none",
-                                              }}
-                                              tabIndex={-1}
-                                            >
-                                              <p
-                                                style={{
-                                                  marginLeft: "0.7em",
-                                                  display: "inline",
-                                                  width: "100%",
-                                                  whiteSpace: "nowrap",
-                                                  overflow: "hidden",
-                                                  textOverflow: "ellipsis",
-                                                  outline: "none",
-                                                }}
-                                              >
-                                                <MARKDOWNIcon />{" "}
-                                                {child.name.slice(0, -3)}
-                                              </p>
-                                            </button>
-                                          </ol>
-                                        ))
-                                        .sort((a, b) => {
-                                          if (
-                                            a.props.children[0]?.props
-                                              .children[1]
-                                          ) {
-                                            return -1;
-                                          } else {
-                                            return 1;
-                                          }
-                                        })}
-                                    </details>
-                                  </div>
-                                )
-                              ) : (
-                                <ol
-                                  className={
-                                    path === child.path && !fileNameBox
-                                      ? "selected files"
-                                      : "greys files"
-                                  }
-                                  onClick={(e) => {
-                                    onFileTreeClick(child.path, child.name);
-                                  }}
-                                  style={{
-                                    cursor: "pointer",
-                                  }}
-                                >
-                                  <button
-                                    style={{
-                                      marginLeft: "1.8em",
-                                      whiteSpace: "nowrap",
-                                      overflow: "hidden",
-                                      maxWidth: "100%",
-                                      outline: "none",
-                                    }}
-                                    tabIndex={-1}
-                                  >
-                                    <p
-                                      style={{
-                                        display: "inline",
-                                        width: "100%",
-                                        whiteSpace: "nowrap",
-                                        overflow: "hidden",
-                                        textOverflow: "ellipsis",
-                                        outline: "none",
-                                      }}
-                                    >
-                                      <MARKDOWNIcon /> {child.name.slice(0, -3)}
-                                    </p>
-                                  </button>
-                                </ol>
-                              )
-                            )}
-                          </details>
-                        )
-                      ) : (
-                        <>
-                          <ol
-                            className={
-                              path === file.path && !fileNameBox
-                                ? "files selected"
-                                : "files greys"
-                            }
-                            onClick={() => {
-                              onFileTreeClick(file.path, file.name);
-                            }}
-                            style={{
-                              cursor: "pointer",
-                            }}
-                          >
-                            <button tabIndex={-1} style={{ outline: "none" }}>
-                              <p
-                                style={{
-                                  marginLeft: "1em",
-                                  outline: "none",
-                                  display: "inline",
-                                  width: "100%",
-                                  whiteSpace: "nowrap",
-                                  overflow: "hidden",
-                                  textOverflow: "ellipsis",
-                                }}
-                              >
-                                <MARKDOWNIcon /> {file.name.slice(0, -3)}
-                              </p>
-                            </button>
-                          </ol>
-                        </>
-                      )
-                    )
-                    .sort((a, b) => {
-                      if (a?.props?.children[0]?.props?.children[1]) {
-                        return -1;
-                      } else {
-                        return 1;
-                      }
-                    })}
 
-                  {fileNameBox ? (
-                    <form
-                      className="files greys selected"
-                      onSubmit={() => {
-                        if (fileName.length < 1) {
-                          setFileNameBox(false);
-                          return;
-                        }
-                        isCreatingFolder
-                          ? createNewDir(fileName)
-                          : createNewFile();
-                        setFileNameBox(false);
-                        setTimeout(() => {
-                          setFileName("");
-                        }, 100);
-                      }}
-                    >
-                      <input
-                        autoFocus
-                        className="createFile"
-                        type="text"
-                        placeholder={
-                          isCreatingFolder ? "Folder Name" : "File Name"
-                        }
-                        onChange={(e) => setFileName(e.target.value)}
-                      />
-                    </form>
-                  ) : null}
+                  <FileTree
+                    struct={struct}
+                    onFileTreeClick={(path, name) => {
+                      onFileTreeClick(path, name);
+                    }}
+                    path={path}
+                    fileNameBox={fileNameBox}
+                    parentDirClick={(path)=>{
+                      setParentDir(path)
+                    }}
+                    creatingFileOrFolder={creatingFileOrFolder}
+                    setFileName={(name) => {
+                      setFileName(name)
+                    }}
+                    isCreatingFolder={isCreatingFolder}
+
+                  />
                 </div>
                 <div
                   className={"fixed util"}
